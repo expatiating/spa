78cf88240ca6ce55693317e82a752000e6546dc6
expatiating <expatiating@gmail.com>
2015-04-25 13:19:58 -0700

    Listing 5.13

--- spa_chapter5/js/spa.js
+++ spa_chapter5/js/spa.js
@@ -2,8 +2,8 @@
  * spa.js
  * Root namespace module
  */
- 
- /*jslint        browser  : true,   continue : true, 
+
+ /*jslint        browser  : true,   continue : true,
   devel  : true,   indent : 2,        maxerr : 50,
   newcap : true,    nomen : true;   plusplus : true,
   regexp : true,   sloppy : true,       vars : false,
@@ -12,7 +12,9 @@
   /*global $, spa */
 
 var spa = (function () {
+  'use strict';
   var initModule = function ( $container ) {
+    spa.model.initModule();
     spa.shell.initModule( $container );
   };
 
