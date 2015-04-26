32140999039037a6feffe1f49ac82db9d668dc58
expatiating <expatiating@gmail.com>
2015-04-25 18:55:41 -0700

    Chapter 5.4

--- spa_chapter5/js/spa.fake.js
+++ spa_chapter5/js/spa.fake.js
@@ -1,7 +1,8 @@
 /*
  * spa.fake.js
  * Fake module
- */
+*/
+
 /*jslint         browser : true, continue : true,
   devel  : true, indent  : 2,    maxerr   : 50,
   newcap : true, nomen   : true, plusplus : true,
@@ -9,37 +10,37 @@
   white  : true
 */
 /*global $, spa */
-spa.fake = (function() {
+
+spa.fake = (function () {
   'use strict';
   var getPeopleList, fakeIdSerial, makeFakeId, mockSio;
 
   fakeIdSerial = 5;
 
-  makeFakeId = function() {
+  makeFakeId = function () {
     return 'id_' + String( fakeIdSerial++ );
   };
 
-  getPeopleList = function() {
+  getPeopleList = function () {
     return [
-      { name: 'Gerry', _id: 'id_01',
-        css_map:
-        { top: 20, left: 20,
-          'background-color': 'rgb( 128, 128, 128)'
+      { name : 'Gerry', _id : 'id_01',
+        css_map : { top: 20, left: 20,
+          'background-color' : 'rgb( 128, 128, 128)'
         }
       },
-      { name: 'Nate', _id: 'id_02',
-        css_map: { top: 60, left: 20,
-          'background-color': 'rgb( 128, 255, 128)'
+      { name : 'Nate', _id : 'id_02',
+        css_map : { top: 60, left: 20,
+          'background-color' : 'rgb( 128, 255, 128)'
         }
       },
-      { name: 'Amy', _id: 'id_03',
-        css_map: { top: 100, left: 20,
-          'background-color': 'rgb( 128, 192, 192)'
+      { name : 'Amy', _id : 'id_03',
+        css_map : { top: 100, left: 20,
+          'background-color' : 'rgb( 128, 192, 192)'
         }
       },
-      { name: 'Danielle', _id: 'id_04',
-        css_map: { top: 140, left: 20,
-          'background-color': 'rgb( 192, 128, 128)'
+      { name : 'Danielle', _id : 'id_04',
+        css_map : { top: 140, left: 20,
+          'background-color' : 'rgb( 192, 128, 128)'
         }
       }
     ];
@@ -55,24 +56,25 @@ spa.fake = (function() {
     emit_sio = function ( msg_type, data ) {
 
       // respond to 'adduser' event with 'userupdate'
-      // callback after 3s delay
+      // callback after a 3s delay
       //
       if ( msg_type === 'adduser' && callback_map.userupdate ) {
-        setTimeout ( function() {
+        setTimeout( function () {
           callback_map.userupdate(
-            [{ _id      : makeFakeId(),
-               name     : data.name,
-               css_map  : data.css_map
+            [{  _id     : makeFakeId(),
+                name    : data.name,
+                css_map : data.css_map
             }]
           );
-        }, 3000 );
+        }, 10000 );
       }
     };
 
     return { emit : emit_sio, on : on_sio };
-  } ());
+  }());
+
   return {
     getPeopleList : getPeopleList,
-    mockSio : mockSio
-   };
+    mockSio       : mockSio
+  };
 }());
