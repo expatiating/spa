4126bfc9cfd2952b61c1097e07a492c67c9e6414
expatiating <expatiating@gmail.com>
2015-04-26 10:55:18 -0700

    Conclusion of Chapter 6.3

--- spa_chapter6/js/spa.model.js
+++ spa_chapter6/js/spa.model.js
@@ -169,11 +169,12 @@ spa.model = (function () {
     };
 
     logout = function () {
-      var is_removed, user = stateMap.user;
+      var user = stateMap.user;
 
       chat._leave();
       is_removed    = removePerson( user );
       stateMap.user = stateMap.anon_user;
+      clearPeopleDb();
 
       $.gevent.publish( 'spa-logout', [ user ] );
       return is_removed;
@@ -250,7 +251,7 @@ spa.model = (function () {
 
     // Begin internal methods
     _update_list = function( arg_list ) {
-      var i, person_map, make_person_map,
+      var i, person_map, make_person_map, person,
         people_list      = arg_list[ 0 ],
         is_chatee_online = false;
 
@@ -275,8 +276,11 @@ spa.model = (function () {
           name    : person_map.name
         };
 
+        person = makePerson( make_person_map );
+
         if ( chatee && chatee.id === make_person_map.id ) {
           is_chatee_online = true;
+          chatee = person;
         }
         makePerson( make_person_map );
       }
